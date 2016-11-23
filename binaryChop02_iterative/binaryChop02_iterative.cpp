9fc1c457f5997ecdb9923150f84696f6b1b81aa2
Manuel Vetterlein <m.vetterlein@inoxision.de>
2016-11-23 11:02:46 +0100

    first version

--- binaryChop02_iterative/binaryChop02_iterative.cpp
+++ binaryChop02_iterative/binaryChop02_iterative.cpp
@@ -2,10 +2,44 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
+int chop(int target, int volume[], int left, int right) {
+
+	int middle;
+	middle = (left + right) / 2;
+	while ((volume[middle] != target)&&(middle!=-1))
+	{
+		middle = (left + right) / 2;
+		if (left>right)
+		{
+			middle = -1;
+		}
+		else if (volume[middle]>target)
+		{
+			right = middle - 1;
+			
+		}
+		else
+		{
+			left = middle + 1;
+			
+		}
+		cout << middle << endl;
+	}
+	return middle+1;
+}
 
 int main()
 {
+	int target = 22;
+	int volume[7] = { 1, 4, 6, 7, 13, 22 ,125};
+	int left = 0;
+	int right;
+	right = (sizeof(volume) / sizeof(*volume));
+	cout << target << " was found at position " << chop(target, volume, left, right);
+	cin >> target;
     return 0;
 }
 
